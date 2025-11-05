531e1d7638c1e031f7ec6ace8c8c4f9fd3c252e4
KingBootleg <moniiette07@hotmail.com>
2025-11-05 13:00:42 -0600

    Creacion de modelos y de la Base de Datos, creacion de .gitignore

--- website/Cafeteria/admin.py
+++ website/Cafeteria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Producto, Vehiculo, Sucursal, Pedido, Comentario
 
 # Register your models here.
+
+admin.site.register(Producto)
+admin.site.register(Vehiculo)
+admin.site.register(Sucursal)
+admin.site.register(Pedido)
+admin.site.register(Comentario)

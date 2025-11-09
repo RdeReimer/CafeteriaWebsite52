efdf0e580211deb85d125fba94690c51cb4fcea1
KingBootleg <moniiette07@hotmail.com>
2025-11-08 21:59:36 -0600

    agregada las vistas, las urls y los modelos

--- website/Cafeteria/models.py
+++ website/Cafeteria/models.py
@@ -28,8 +28,9 @@ class Vehiculo(models.Model):
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
+    descripcion = models.TextField(blank=True)
     precio = models.DecimalField(max_digits=6, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/', blank=True)
 
     def __str__(self):
         return self.nombre

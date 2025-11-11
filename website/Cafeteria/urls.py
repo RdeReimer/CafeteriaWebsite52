fb531950e6374e2834c01e4042a869e338bc3415
Andres Leal <andreslealgzz@hotmail.com>
2025-11-11 16:51:03 -0600

    funcionamiento basico de htmls

--- website/Cafeteria/urls.py
+++ website/Cafeteria/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('productos/' views.productos, name='productos'),
-    path('eventos/' views.eventos, name='eventos'),
-    path('autos/', views.autos, name='autos'),
-    path('sucursales/', views.sucursales, name='sucursales'),
-    path('comentarios/', views.comentarios, name='comentarios'),
+#     path('productos/' views.productos, name='productos'),
+#     path('eventos/' views.eventos, name='eventos'),
+#     path('autos/', views.autos, name='autos'),
+#     path('sucursales/', views.sucursales, name='sucursales'),
+#     path('comentarios/', views.comentarios, name='comentarios'),
+# 
 ]
--- website/Cafeteria/views.py
+++ website/Cafeteria/views.py
@@ -4,24 +4,24 @@ from .models import Producto, Evento, Sucursal, Vehiculo, Comentario
 # Create your views here.
 
 def inicio(request):
-    return render(request, 'index.html')
+    return render(request, 'cafeteria.html')
 
-def productos(request):
-    productos = Producto.objects.all()
-    return render(request, 'productos.html', {'productos': productos})
+# def productos(request):
+#     productos = Producto.objects.all()
+#     return render(request, 'productos.html', {'productos': productos})
 
-def eventos(request):
-    eventos = Evento.objects.all()
-    return render(request, 'eventos.html', {'eventos': eventos})
+# def eventos(request):
+#     eventos = Evento.objects.all()
+#     return render(request, 'eventos.html', {'eventos': eventos})
 
-def autos(request):
-    autos = Vehiculo.objects.all()
-    return render(request, 'autos.html', {'autos':autos})
+# def autos(request):
+#     autos = Vehiculo.objects.all()
+#     return render(request, 'autos.html', {'autos':autos})
 
-def sucursales(request):
-    sucursales = Sucursal.objects.all()
-    return render(request, 'sucursales.html', {'sucursales': sucursales})
+# def sucursales(request):
+#     sucursales = Sucursal.objects.all()
+#     return render(request, 'sucursales.html', {'sucursales': sucursales})
 
-def comentarios(request):
-    comentarios = Comentario.objects.all()
-    return render(request, 'comentarios.html', {'comentarios': comentarios})
+# def comentarios(request):
+#     comentarios = Comentario.objects.all()
+#     return render(request, 'comentarios.html', {'comentarios': comentarios})
